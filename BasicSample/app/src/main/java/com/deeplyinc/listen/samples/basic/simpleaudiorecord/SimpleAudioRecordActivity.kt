388de8c717cb6e9c74963c7912cc228b8a9bda23
Han Park <han@deeplyinc.com>
2022-08-16 14:28:44 +0900

    feat: add SimpleAudioRecordActivity example

--- BasicSample/app/src/main/java/com/deeplyinc/listen/samples/basic/simpleaudiorecord/SimpleAudioRecordActivity.kt
+++ BasicSample/app/src/main/java/com/deeplyinc/listen/samples/basic/simpleaudiorecord/SimpleAudioRecordActivity.kt
@@ -1,18 +1,118 @@
 package com.deeplyinc.listen.samples.basic.simpleaudiorecord
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.deeplyinc.listen.samples.basic.R
 import com.deeplyinc.listen.samples.basic.databinding.ActivitySimpleAudioRecordBinding
+import com.deeplyinc.listen.sdk.Listen
+import com.deeplyinc.listen.sdk.audio.classifiers.datastructures.ClassifierOutput
+import com.deeplyinc.listen.sdk.exceptions.ListenAuthException
+import com.deeplyinc.recorder.DeeplyRecorder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SimpleAudioRecordActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "SimpleAudioRecordActivity"
+    }
+
     private lateinit var binding: ActivitySimpleAudioRecordBinding
+
+    private val listen = Listen(this)
+    private lateinit var audioRecord: AudioRecord
+    private var isRecording = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_simple_audio_record)
+        binding.lifecycleOwner = this
 
+        initialize()
 
+        configureLayout()
+
+        DeeplyRecorder.requestRecordingPermission(this) {
+            Log.d(TAG, "Recording permission granted ")
+        }
+    }
+
+    private fun initialize() {
+        binding.start.isEnabled = false
+        // Note that the init() takes time and blocks the thread during the initialization
+        // process because it contains networking and file operations.
+        // We recommend to call init() in the other thread like the following code.
+        lifecycleScope.launch(Dispatchers.Default) {
+            try {
+                listen.init("SDK KEY", "DPL ASSET PATH")
+
+                withContext(Dispatchers.Main) {
+                    binding.start.isEnabled = true
+                }
+            } catch (e: ListenAuthException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun configureLayout() {
+        binding.start.setOnClickListener {
+            if (isRecording) {
+                stopRecording()
+                binding.start.text = "Start"
+            } else {
+                startRecording()
+                binding.start.text = "Stop"
+            }
+        }
+    }
+
+    private fun startRecording() {
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        val bufferSize = listen.getAudioParams().inputSize
+        val buffer = ShortArray(bufferSize)
+        val sampleRate = listen.getAudioParams().sampleRate
+        audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize)
+        audioRecord.startRecording()
+        isRecording = true
+        lifecycleScope.launch(Dispatchers.Default) {
+            while (isRecording) {
+                // called every 0.1 second, buffer contains 1,000 samples
+                audioRecord.read(buffer, 0, buffer.size)
+                val result = listen.inference(buffer)
+                withContext(Dispatchers.Main) {
+                    handleResult(result)
+                }
+            }
+        }
+    }
+
+    private fun stopRecording() {
+        isRecording = false
+        audioRecord.stop()
+        audioRecord.release()
+    }
+
+    private fun handleResult(result: ClassifierOutput) {
+        // print result
+        Log.d(TAG, "Inference result: ${result.event} ${result.confidence}")
+        Log.d(TAG, "All results: ${result.rawResults}")
+
+        // update UI
+        binding.event.text = result.event
+        binding.confidence.text = String.format("%.2f%%", result.confidence * 100.0)
     }
 }
--- BasicSample/app/src/main/java/com/deeplyinc/listen/samples/basic/simpledeeplyrecorder/SimpleDeeplyRecorderActivity.kt
+++ BasicSample/app/src/main/java/com/deeplyinc/listen/samples/basic/simpledeeplyrecorder/SimpleDeeplyRecorderActivity.kt
@@ -4,13 +4,17 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.deeplyinc.listen.samples.basic.R
 import com.deeplyinc.listen.samples.basic.databinding.ActivitySimpleDeeplyRecorderBinding
+import com.deeplyinc.listen.sdk.AudioEventClassificationListener
 import com.deeplyinc.listen.sdk.Listen
+import com.deeplyinc.listen.sdk.audio.classifiers.datastructures.ClassifierOutput
+import com.deeplyinc.listen.sdk.exceptions.ListenAuthException
 import com.deeplyinc.recorder.DeeplyRecorder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -23,27 +27,52 @@ class SimpleDeeplyRecorderActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySimpleDeeplyRecorderBinding
 
-    private lateinit var listen: Listen
+    private val listen = Listen(this)
     private lateinit var recorder: DeeplyRecorder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) // Prevent screen off
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_simple_deeply_recorder)
         binding.lifecycleOwner = this
 
         initialize()
 
         configureLayout()
+
+        DeeplyRecorder.requestRecordingPermission(this) {
+            Log.d(TAG, "Recording permission granted ")
+        }
     }
 
     private fun initialize() {
-        listen = Listen(this)
-        listen.init("SDK KEY HERE", ".dpl FILE ASSETS PATH HERE")
+        binding.start.isEnabled = false
+        // Note that the init() takes time and blocks the thread during the initialization
+        // process because it contains networking and file operations.
+        // We recommend to call init() in the other thread like the following code.
+        lifecycleScope.launch(Dispatchers.Default) {
+            try {
+                listen.init("SDK KEY", "DPL ASSET PATH")
 
-        recorder = DeeplyRecorder(
-            sampleRate = listen.getAudioParams().sampleRate,
-            bufferSize = listen.getAudioParams().inputSize
-        )
+                recorder = DeeplyRecorder(
+                    sampleRate = listen.getAudioParams().sampleRate,
+                    bufferSize = listen.getAudioParams().inputSize
+                )
+
+                withContext(Dispatchers.Main) {
+                    binding.start.isEnabled = true
+                }
+
+                listen.setAudioEventClassificationListener(object : AudioEventClassificationListener {
+                    override fun onDetected(result: ClassifierOutput) {
+                        handleResult(result)
+                    }
+                })
+            } catch (e: ListenAuthException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun configureLayout() {
@@ -64,14 +93,7 @@ class SimpleDeeplyRecorderActivity : AppCompatActivity() {
                 Manifest.permission.RECORD_AUDIO
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            DeeplyRecorder.requestRecordingPermission(this) {
-                Log.d(TAG, "Recording permission granted ")
-                lifecycleScope.launch {
-                    recorder.start().collect {
-                        runInference(it)
-                    }
-                }
-            }
+            Log.w(TAG, "Recording permission is not granted ")
             return
         } else {
             lifecycleScope.launch {
@@ -85,15 +107,18 @@ class SimpleDeeplyRecorderActivity : AppCompatActivity() {
     private suspend fun runInference(audioSamples: ShortArray) {
         // run inference
         val result = listen.inference(audioSamples)
+        withContext(Dispatchers.Main) {
+            handleResult(result)
+        }
+    }
 
+    private fun handleResult(result: ClassifierOutput) {
         // print result
         Log.d(TAG, "Inference result: ${result.event} ${result.confidence}")
-        Log.d(TAG, "All results: $result")
+        Log.d(TAG, "All results: ${result.rawResults}")
 
         // update UI
-        withContext(Dispatchers.Main) {
-            binding.event.text = result.event
-            binding.confidence.text = String.format("%.2f", result.confidence * 100.0)
-        }
+        binding.event.text = result.event
+        binding.confidence.text = String.format("%.2f%%", result.confidence * 100.0)
     }
 }
